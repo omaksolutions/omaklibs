669daeccc75e69a42b07a29bd87c9b397e0308cb
Ankit Singla <ak.singla@hotmail.com>
2020-07-24 18:50:46 +0530

    Initial setup

---
+++ app/src/main/java/com/omak/omaklibs/MainActivity.java
@@ -0,0 +1,14 @@
+package com.omak.omaklibs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
