17db354750914ae062debe5dfd975ee42a1e9e87
Ankit Singla <ak.singla@hotmail.com>
2020-08-03 16:00:11 +0530

    Fixed: Preference & Notification Log

--- SampleLibrary/src/main/java/com/omak/omakhelpers/PreferencesHelper.java
+++ SampleLibrary/src/main/java/com/omak/omakhelpers/PreferencesHelper.java
@@ -2,6 +2,10 @@ package com.omak.omakhelpers;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import static android.content.Context.MODE_PRIVATE;
 
@@ -21,6 +25,26 @@ public class PreferencesHelper {
     public void setPrefName(String pref_name) {
         PREFS_NAME = pref_name;
         preferences = context.getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+    }
+
+    /*Getting Key From This Helper Class*/
+    public static String getPrefString(String key, String variable, Context context) {
+        String variableString = preferences.getString(variable, "");
+
+        // Return if the preferences does not have requested variable stored
+        if (variableString.isEmpty()) return null;
+
+        try {
+            JSONObject variableObj = new JSONObject(variableString);
+            if (!key.isEmpty()) {
+                String keyString = variableObj.has(key) ? variableObj.getString(key) : "";
+                return keyString;
+            }
+        } catch (final JSONException e) {
+            HelperFunctions.theLogger("JSONException", "Error for preference key: " + key + " & variable: " + variable + " with error: " + e.getMessage());
+            return null;
+        }
+        return null;
     }
 
     public static boolean getPreference(Context context, String key) {
