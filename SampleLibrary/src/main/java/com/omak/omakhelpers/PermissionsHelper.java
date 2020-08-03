4996952a63f70d39a23fbe4998d904f073cb2dd3
Ankit Singla <ak.singla@hotmail.com>
2020-08-03 17:57:07 +0530

    Added: PermissionsHelper 2

--- SampleLibrary/src/main/java/com/omak/omakhelpers/PermissionsHelper.java
+++ SampleLibrary/src/main/java/com/omak/omakhelpers/PermissionsHelper.java
@@ -1,27 +1,19 @@
 package com.omak.omakhelpers;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Build;
-import android.provider.Settings;
 
 import androidx.core.app.ActivityCompat;
 
-import com.omak.samplelibrary.R;
-
 import java.util.ArrayList;
-import java.util.EventListener;
 
 public class PermissionsHelper {
     public static final int REQUEST_PERMISSION_MULTIPLE = 0;
+    public static final int REQUEST_PERMISSION_SINGLE = 1;
+    public EventListener listener;
     private Activity activity;
     private String[] permissions;
-    public EventListener listener;
 
     public PermissionsHelper(Activity activity, String[] permissions, EventListener listener) {
         this.activity = activity;
@@ -29,29 +21,21 @@ public class PermissionsHelper {
         this.permissions = permissions;
     }
 
-    public interface EventListener {
-        Boolean onPermissionsGranted(Integer grantedPermissions, Integer totalPermissions, ArrayList<String> permissionNotGranted);
-    }
-
-    public boolean hasPermissions(Context context) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
-            for (String permission : permissions) {
-                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
     public String[] getRequiredPermissionsArray() {
         return this.permissions;
     }
 
     //intent for open run time permission
-    private void openRuntimePermission() {
+    public void openRuntimePermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
             ActivityCompat.requestPermissions(activity, permissions, REQUEST_PERMISSION_MULTIPLE);
+        }
+    }
+
+    //intent for open run time permission
+    public void openRuntimePermission(String permission) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            ActivityCompat.requestPermissions(activity, new String[]{permission}, REQUEST_PERMISSION_SINGLE);
         }
     }
 
@@ -63,18 +47,28 @@ public class PermissionsHelper {
         Integer grantedPermissions = 0;
         ArrayList<String> permissionNotGranted = new ArrayList<String>();
 
+        if (grantResults != null) {
+            for (int i = 0; i < permissions.length; i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    grantedPermissions++;
+                } else {
+                    permissionNotGranted.add(permissions[i]);
+                }
+            }
+        }
         switch (requestCode) {
             case 1:
-                if (grantResults.length < 1) return;
-                for (int i = 0; i < permissions.length; i++) {
-                    if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
-                        grantedPermissions++;
-                    } else {
-                        permissionNotGranted.add(permissions[i]);
-                    }
-                }
+                listener.onPermissionsGranted(grantedPermissions, totalPermissions, permissionNotGranted);
+                break;
+            case 2:
+                listener.onPermissionsGrantedSingle(permissions[0]);
+                break;
         }
 
-        listener.onPermissionsGranted(grantedPermissions, totalPermissions, permissionNotGranted);
+    }
+
+    public interface EventListener {
+        void onPermissionsGranted(Integer grantedPermissions, Integer totalPermissions, ArrayList<String> permissionNotGranted);
+        void onPermissionsGrantedSingle(String permission);
     }
 }
